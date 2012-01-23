0f4fd9de1f29fe76c50d3ddf6698a72d79bffcbc
Julien Girault <julien.emac@gmail.com>
2012-01-23 14:15:40 +0100

    last update

--- lib/ShareX/App.pm
+++ lib/ShareX/App.pm
@@ -4,8 +4,7 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 my $isAdmin = false;
-#my $user = $ENV{'REMOTE_USER'};
-my $user = 'Julien Girault';
+my $user = $ENV{'REMOTE_USER'};
 
 get '/' => sub {
 	template 'index', { isAdmin => $isAdmin, user=>$user };
@@ -31,20 +30,6 @@ get '/test' => sub {
 post '/upload' => sub {
 
 	my $filename = param('file');
-	my $message;
-	
-	my $upload = upload( $filename );
-   # $filename = request->filename; # request, params parsed as a hash ref
-    my %params = request->params; # request, params parsed as a hash ref
-    my $request = request->body; # returns the request body, unparsed
-    my $path = request->path; # the path requested by the client
-	for (keys %params)
-    {
-        $message .= "$_ => $params{$_}<br>";
-    }
-
-        
-	template 'index', { isAdmin => $isAdmin, user=>$filename, message=>$message};
     	
 };
     
