c54c398335cf2eea45aea28e5e72ff55c1460d3a
Julien Girault <julien.emac@gmail.com>
2012-01-23 16:19:13 +0100

    new app.pm

--- lib/ShareX/App.pm
+++ lib/ShareX/App.pm
@@ -6,31 +6,31 @@ our $VERSION = '0.1';
 my $isAdmin = false;
 my $user = $ENV{'REMOTE_USER'};
 
+#------------------------------------------------------------
+# Les routes
+#------------------------------------------------------------
+
+#----------------------
+# Partie commune
+#----------------------
+
+#-- Déposer un fichier
 get '/' => sub {
 	template 'index', { isAdmin => $isAdmin, user=>$user };
 };
-
-get '/admin' => sub {
-	template 'admin', { isAdmin => $isAdmin };
-};
-
-get '/test' => sub {
-	template
-};
-
-#get '/test/:param1/:param2/:param3/:param4' => sub {
-#	my $nom = param('param1');
-#	template 'test', { 
-#		nom => $nom,
-#    	age => $age,
-#    	sexe => $sexe,
-#    	pouf => $pouf };
-#};
-
 post '/upload' => sub {
 
 	my $filename = param('file');
     	
 };
     
+#----------------------
+# Administration
+#----------------------
+
+#-- Index administration
+get '/admin' => sub {
+	template 'admin', { isAdmin => $isAdmin };
+};
+
 true;
